26ddd097e86cd19162fc2ee57dfe097915b5db8c
IagoR7 <iago85210@gmail.com>
2025-04-21 18:39:50 -0300

    Add files via upload

---
+++ Environment/Environment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseEnvironment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            //Environment = Ambiente
+            string meuDocs =  Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            Environment.CurrentDirectory = "C:\\";
+            string dirAtual = Environment.CurrentDirectory;
+
+            //Environment.NewLine; quebra de linha
+            int num1 = 2;
+            
+            string varAmb = Environment.GetEnvironmentVariable("Path");
+
+
+            //List<string> discos = Environment.GetLogicalDrives().ToList();
+
+            /*string[] discos = Environment.GetLogicalDrives();
+            foreach (var item in discos)
+            {
+                Console.WriteLine("\n" + item);
+            }*/
+
+            string user = Environment.UserName;
+            string Dominio = Environment.UserDomainName;
+            int cpu =  Environment.ProcessorCount;
+            
+
+            Console.WriteLine(cpu);
+
+
+            Console.ReadLine();
+        }
+    }
+}
