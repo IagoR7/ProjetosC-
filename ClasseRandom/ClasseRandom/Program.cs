36617cbc31896e21e083cb4eecb8daa6b52de125
IagoR7 <iago85210@gmail.com>
2025-04-21 18:52:18 -0300

    Add files via upload

---
+++ ClasseRandom/ClasseRandom/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClasseRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aleatorio();
+            RRussa();
+        }
+
+        static void Aleatorio()
+        {   
+            Random R = new Random(DateTime.Now.Millisecond);
+            //(100) vai até 99 pq o 100 ele exclui / (0,10) valor minino, valor maximo
+            int valor = R.Next(99);
+            double valor2 = R.NextDouble();
+            Console.WriteLine(valor);
+            Console.ReadLine();
+        }
+        static async void RRussa()
+        {
+            Random r = new Random();
+            int valor = r.Next(1,7);
+            int resultado = 0;
+
+            while (valor != resultado)
+            {
+                Console.Write("Escolhea um numero entre 1 a 6 : ");
+                resultado = int.Parse(Console.ReadLine());
+
+                if (resultado == valor)
+                {
+                    
+                    Console.Write("...\n...\n...\n... ");
+                    
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Girando o Tambor novamente");
+                    Thread.Sleep(1000);
+
+                    
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
