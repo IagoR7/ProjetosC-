4f90888ffd662dae977228907be1032d9b9f8836
IagoR7 <iago85210@gmail.com>
2025-04-21 18:55:27 -0300

    Add files via upload

---
+++ ClonarString/ClonarString/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ClonarString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnexecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(709, 294);
+            this.label1.TabIndex = 0;
+            // 
+            // btnexecutar
+            // 
+            this.btnexecutar.Location = new System.Drawing.Point(332, 360);
+            this.btnexecutar.Name = "btnexecutar";
+            this.btnexecutar.Size = new System.Drawing.Size(378, 53);
+            this.btnexecutar.TabIndex = 1;
+            this.btnexecutar.Text = "executar";
+            this.btnexecutar.UseVisualStyleBackColor = true;
+            this.btnexecutar.Click += new System.EventHandler(this.btnexecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnexecutar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnexecutar;
+    }
+}
+
---
+++ ClonarString/ClonarString/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClonarString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnexecutar_Click(object sender, EventArgs e)
+        {
+            #region contains/Tostring
+            /*string texto = "Este de Variavel é alfa numerica. ";
+            if (texto.Contains("Var"))
+            {
+                label1.Text = "Contém";
+            }
+            else
+            {
+                label1.Text = "Não Contém";
+            }*/
+
+            /*int num = 5400;
+            bool res = true;
+
+            /*label1.Text = num.ToString();
+            label1.Text = res.ToString();*/
+            #endregion
+
+            #region Toupper / Tolower
+            /*string nome = "iago";
+
+            nome = nome.ToUpper();
+            string nomeMai = nome.ToUpper();
+            string nomeMin = nome.ToLower();
+
+            label1.Text = nome; */
+
+            #endregion
+
+            #region IndexOF
+            //string nome = "iago ribeiro ";
+
+            //int indice = nome.IndexOf('o');// primeira ocorrencia
+            // int indice = nome.IndexOf('o',5);//vai percorrer a partir do
+            // int indice = nome.IndexOf('o',5,4);//vai percorrer apenas 4 caracteres
+
+            //int indice = nome.LastIndexOf('a');//ultima ocorrencia
+            //label1.Text  = "Indice: " + indice;
+            #endregion
+
+            #region Insert / replace
+            /* string nome = "Iago Ribeiro ";
+
+             //string nomeFinal = nome.Insert(4, " Ribeiro ");
+             //string nomeFinal = nome.Replace('o','X');
+             string nomeFinal = nome.Replace("Ribeiro", "Santos");
+             label1.Text = nomeFinal;*/
+            #endregion
+
+            #region Length / Substring 
+
+            //string nome = "Iago Ribeiro";
+
+            //int tamanho = nome.Length;
+            //label1.Text = " o nome " + nome + " contem " + tamanho + " Letras";
+
+            //for (int i = 0; i < nome.Length; i++)
+            //{
+            //    label1.Text += nome[i] + "\n";
+            //}
+
+            //string parte = nome.Substring(0, 4);
+            //string parte = nome.Substring(5);
+            //string parte = nome.Substring(nome.IndexOf(" "));
+            //label1.Text = parte;
+            #endregion
+
+            #region split
+            /*string nomes = "Iago+_+Arthur+_+Sunny+_+Kimdokja";
+
+            //char[] separador1 = { ' ', ' - ', ' _ ' }; 
+            string[] separador2 = {"+_+"};
+            //string[] resultado = nomes.Split(separador1);
+            string[] resultado = nomes.Split(separador2, StringSplitOptions.None);
+
+            foreach (string nome in resultado) {
+
+                label1.Text += nome + "\n";
+            }*/
+            #endregion
+
+            #region StartsWith / EndsWith
+            /*string nome = "Iago";
+            //bool res = nome.StartsWith("I");
+            bool res = nome.StartsWith("i", StringComparison.OrdinalIgnoreCase);
+            bool res = nome.EndsWith("GO", StringComparison.OrdinalIgnoreCase );
+
+            if (res) 
+            {
+                label1.Text = "Positivo";
+            }
+            else
+            {
+                label1.Text = "Negativo";
+            }*/
+
+            #endregion
+
+            #region Trim - TrimStart - TrimEnd
+            //string mensagem = "      Olá Iago        ";
+
+            //char[] c = { ' ' , '-' , '_', '+'};
+
+            //string nova = mensagem.Trim(c);
+            //mensagem.TrimStart()
+
+
+            //label1.Text = ">" + mensagem.TrimEnd() + "<";
+
+
+
+            #endregion
+
+            #region CompareTo - Equals
+            // fazer comparações entre as strings
+
+            //string nome = "Iago";
+            //string nome2 = "iago";
+            /*if (nome.Equals(nome2, StringComparison.OrdinalIgnoreCase))
+            {
+                label1.Text = "positivo";
+            }
+            else
+            {
+                label1.Text = "Negativo";
+            }*/
+
+            //label1.Text = nome.CompareTo(nome2).ToString();//0 para igual, 1 para anterior, -1 para posterior
+
+
+
+            #endregion
+
+            #region Format
+            decimal valor = 19.95m;
+
+            //string s = String.Format("O valor do Produto é {0:C2}",valor);
+            string s = String.Format($"Hoje é {DateTime.Now:D} e são {DateTime.Now:T} ");
+
+            label1.Text = s;
+
+            #endregion
+
+        }
+    }
+}
