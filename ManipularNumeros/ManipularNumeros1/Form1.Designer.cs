8491bf576daf3a437681f9b991ef56c79aae8d7e
IagoR7 <iago85210@gmail.com>
2025-04-21 18:53:55 -0300

    Add files via upload

---
+++ ManipularNumeros/ManipularNumeros1/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ManipularNumeros1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnexecutar = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnexecutar
+            // 
+            this.btnexecutar.Location = new System.Drawing.Point(252, 180);
+            this.btnexecutar.Name = "btnexecutar";
+            this.btnexecutar.Size = new System.Drawing.Size(138, 54);
+            this.btnexecutar.TabIndex = 0;
+            this.btnexecutar.Text = "Executar";
+            this.btnexecutar.UseVisualStyleBackColor = true;
+            this.btnexecutar.Click += new System.EventHandler(this.btnexecutar_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(12, 9);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(248, 147);
+            this.lblresultado.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 246);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btnexecutar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnexecutar;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ ManipularNumeros/ManipularNumeros1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularNumeros1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnexecutar_Click(object sender, EventArgs e)
+        {
+            #region TryParse
+            //int num1 = int.Parse("1700");
+            //int num;
+            //bool res = int.TryParse("1711WOW", out num);
+
+            //if (int.TryParse("1711", out num))
+            //{
+            //lblresultado.Text = num.ToString();
+            //}
+            //else
+            //{
+            //lblresultado.Text = "Erro Na conversão";
+            //}
+            #endregion
+
+            #region ToString
+            //int num = 777;
+            //lblresultado.Text = num.ToString("00000.00");
+            //lblresultado.Text = num.ToString("#.00");
+
+            //int num = 11;
+            //lblresultado.Text = num.ToString("x");// hexadecimal
+            //lblresultado.Text = num.ToString("x2");// hexadecimal min 2 digitos
+            //lblresultado.Text = num.ToString("D");// decimal
+            //lblresultado.Text = num.ToString("D5");// decimal min  5 digitos
+            //lblresultado.Text = num.ToString("C");// Monetario
+            //lblresultado.Text = Convert.ToString(num, 16);//base 16
+            #endregion
+
+            #region Math
+            /*double pi  = Math.PI;
+            double E = Math.E;
+            double Seno = Math.Sin(3);
+            double Conseno = Math.Cos(3);
+            double Potencia = Math.Pow(3,2);
+            double RaizQuadrada = Math.Sqrt(8);
+            double arrendonda = Math.Round(12.17);
+            double Inteiro = Math.Truncate(101.18);
+            double Floor = Math.Floor(150.70); arrendonda para baixo
+            double Ceil = Math.Ceiling(150.20); arredonda para cima
+
+
+            lblresultado.Text = pi.ToString("#.00");*/
+
+            #endregion
+        }
+
+    }
+}
