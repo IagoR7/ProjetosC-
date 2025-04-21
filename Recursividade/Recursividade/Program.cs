049bd89ef42dbd6456cb987bb75945b152092319
IagoR7 <iago85210@gmail.com>
2025-04-21 18:56:39 -0300

    Add files via upload

---
+++ Recursividade/Recursividade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            recursivo r = new recursivo();
+            r.executar("Olá mundo", 5 );
+
+            r.ExecutarRecursivo("olá mundo", 5);
+
+            Console.ReadKey();
+
+        }
+    }
+}
